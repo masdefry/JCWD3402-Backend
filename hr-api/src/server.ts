b0ad1733616cf7ce458811e868dcae4812a38938
masdefry <muhammaddefryan@gmail.com>
2025-08-15 17:03:18 +0700

    add error handler middleware

--- hr-api/src/server.ts
+++ hr-api/src/server.ts
@@ -1,10 +1,19 @@
-import express, { Express } from 'express';
+import express, { Express, NextFunction, Request, Response } from 'express';
 import route from './routes/main.route';
 
 const port: number = 8000;
 const app: Express = express();
 app.use(express.json());
 app.use(route);
+
+// Error handler middleware
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({
+    success: false,
+    message: err?.message,
+    data: {},
+  });
+});
 
 app.listen(port, () => {
   console.log(`[API] Running in port: ${port}`);
